e96420c62fe6703242545bf17721ee3d2ea275b9
Ishaq Akacem <104324773+isakus01@users.noreply.github.com>
2022-04-24 17:21:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
-import streamlit st
+import streamlit as st
 
 st.title('My Mom\'s New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Bleuberry Oatmeal')
 st.text('Omega 3 & Bleuberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled 3 Free-Range')
