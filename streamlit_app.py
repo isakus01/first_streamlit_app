d136ab9a3e02573c7898b391ea8f659452fab5ce
Ishaq Akacem <104324773+isakus01@users.noreply.github.com>
2022-04-24 17:21:03 -0400

    Update streamlit_app.py
    Add the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
-import streamlit
+import streamlit st
 
-streamlit.title('My Mom\'s New Healthy Diner')
+st.title('My Mom\'s New Healthy Diner')
+
+st.header('Breakfast Menu')
+st.text('Omega 3 & Bleuberry Oatmeal')
+st.text('Omega 3 & Bleuberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled 3 Free-Range')
+
