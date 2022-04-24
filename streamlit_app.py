bf614359c1d4862a7b297cfd75a6fb5317bf06e0
Ishaq Akacem <104324773+isakus01@users.noreply.github.com>
2022-04-24 17:23:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title('My Mom\'s New Healthy Diner')
 
-st.header('Breakfast Menu')
-st.text('Omega 3 & Bleuberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled 3 Free-Range')
-
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Bleuberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled 3 Free-Range')
+st.text('🥑🍞 Avocado Toast')
